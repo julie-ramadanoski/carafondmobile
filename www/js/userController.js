11688f5eb25a2c76a4031abb8418d2988b12c6f2
Julie Ramadanoski <ramadanoski.j@gmail.com>
2016-03-16 23:26:48 +0100

    liste des alertes

--- www/js/userController.js
+++ www/js/userController.js
@@ -9,20 +9,19 @@
         .controller('UserController', UserController);  
 
 
-    function UserController($http, $auth, $rootScope, $scope) {
+    function UserController($http, $auth, $rootScope, $scope, $state) {
 
         var vm = this;
-        vm.ville;
+        vm.villeDepart;
+        vm.alertes;
         vm.users;
         vm.error;
 
         vm.getAlertes = function(){
 
-            $http.get('http://localhost:8000/api/authenticate/alertes' )
-            //$http.get('http://localhost:8000/api/authenticate/alertes?token=' + localStorage.getItem('satellizer_token') )
+            $http.get('http://localhost:8000/api/authenticate/alertes/'+ vm.villeDepart )
             .success(function(alertes) {
                 vm.alertes = alertes;
-                console.log(alertes);
             })
             
             .error(function(error) {
@@ -31,39 +30,37 @@
         }
 
         vm.getUsers = function() {
-
-
             //Grab the list of users from the API
             $http.get('http://localhost:8000/api/authenticate')
 
             .success(function(users) {
                 vm.users = users;
-                console.log(users);
             })
             
             .error(function(error) {
                 vm.error = error;
             });
         }
+           
+        // We would normally put the logout method in the same
+        // spot as the login method, ideally extracted out into
+        // a service. For this simpler example we'll leave it here
+        vm.logout = function() {
 
-        vm.getAlertes= function(){
-            
-            //Grab the list of alertes from the API
-            console.log($auth.isAuthenticated());   
-          /*  $http({
-                method: 'GET', 
-                url: 'http://localhost:8000/api/authenticate/alertes?access_token='+localStorage.satellizer_token, 
-                headers: {'Authorization': 'Bearer '+localStorage.satellizer_token}
-            })*/
-            $http.get('http://localhost:8000/api/authenticate/alertes')   
-            .success(function(alertes) {
-                console.log(alertes);
-                $rootScope.alertes = alertes;
-            })
-            
-            .error(function(error) {
-                $rootScope.error = error;
+            $auth.logout().then(function() {
+
+                // Remove the authenticated user from local storage
+                localStorage.removeItem('user');
+
+                // Flip authenticated to false so that we no longer
+                // show UI elements dependant on the user being logged in
+                $rootScope.authenticated = false;
+
+                // Remove the current user info from rootscope
+                $rootScope.currentUser = null;
+
+                $state.go('auth');
             });
-        }        
+        }
     }
 })();
